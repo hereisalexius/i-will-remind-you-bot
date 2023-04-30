2c7ee712c07777bc9f3d9e6f03b7cdfae68f1fa5
Oleksii <hereisalexius@gmail.com>
2023-04-30 22:32:21 +0000

    #2 Add telbot support

--- cmd/bot.go
+++ cmd/bot.go
@@ -1,20 +1,27 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	tele "gopkg.in/telebot.v3"
+)
+
+var (
+	BotToken = os.Getenv("BOT_TOKEN")
 )
 
 // botCmd represents the bot command
 var botCmd = &cobra.Command{
-	Use:   "bot",
+	Use:     "bot",
 	Aliases: []string{"start"},
-	Short: "A brief description of your command",
+	Short:   "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -22,7 +29,26 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("bot called")
+		pref := tele.Settings{
+			URL:    "",
+			Token:  BotToken,
+			Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+		}
+
+		b, err := tele.NewBot(pref)
+
+		if err != nil {
+			log.Fatalf("Please check BOT_TOKEN env variable. %s", err)
+			return
+		}
+
+		b.Handle("/hello", func(c tele.Context) error {
+			return c.Send("Hello!")
+		})
+
+		fmt.Printf("i-will-remind-you-bot %s started\n", version)
+		b.Start()
+
 	},
 }
 
