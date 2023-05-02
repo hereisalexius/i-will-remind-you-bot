e8d9f35bf48f5681fcacf0266edf00df82b02eb9
Oleksii <hereisalexius@gmail.com>
2023-05-02 12:11:20 +0000

    Add /set option
    Resolves #6

--- cmd/bot.go
+++ cmd/bot.go
@@ -14,8 +14,16 @@ import (
 )
 
 var (
-	TeleToken = os.Getenv("TELE_TOKEN")
+	TeleToken     = os.Getenv("TELE_TOKEN")
+	messagesCache = make(map[string]*MessageToRemind)
 )
+
+type MessageToRemind struct {
+	//I'm lazy to make a proper handler, lets store whole context to send remind messages
+	UserContext tele.Context
+	MessageText string
+	RimindTime  time.Time
+}
 
 // botCmd represents the bot command
 var botCmd = &cobra.Command{
@@ -42,13 +50,55 @@ to quickly create a Cobra application.`,
 			return
 		}
 
-		b.Handle("/hello", func(c tele.Context) error {
-			return c.Send("Hello!")
+		b.Handle("/set", func(c tele.Context) error {
+			var senderUsername = c.Sender().Username
+
+			messagesCache[senderUsername] = new(MessageToRemind)
+			messagesCache[senderUsername].UserContext = c
+
+			log.Printf("New reminder has been initiated for %s\n", senderUsername)
+
+			return c.Send("What I should remind you?")
+		})
+
+		b.Handle(tele.OnText, func(c tele.Context) error {
+			var senderUsername = c.Sender().Username
+			payload := c.Message().Payload
+			text := c.Text()
+
+			log.Println(payload, c.Text())
+
+			var remindMsg = messagesCache[senderUsername]
+
+			if remindMsg == nil {
+				log.Printf("%s has not set any reminders. Treated as note.\n", senderUsername)
+				return err
+			}
+
+			if remindMsg.MessageText == "" {
+				remindMsg.MessageText = text
+				log.Printf("%s has set message text: %s\n", senderUsername, remindMsg.MessageText)
+				return c.Send("After how long to warn you?\n*Please set time duration like : 1h2m30s")
+			}
+
+			if remindMsg.RimindTime.IsZero() {
+				reminderDuration, err := time.ParseDuration(text)
+
+				if err != nil {
+					return c.Send("Unable to set duration. Please try another one.")
+				}
+				remindMsg.RimindTime = time.Now().Add(reminderDuration)
+
+				log.Printf("%s has set time to: %s\n", senderUsername, remindMsg.RimindTime)
+				log.Printf("Reminder for %s created!\n", senderUsername)
+				return c.Send("Done.")
+			}
+
+			return err
 		})
 
 		fmt.Printf("i-will-remind-you-bot %s started\n", version)
 		b.Start()
-
 	},
 }
 
